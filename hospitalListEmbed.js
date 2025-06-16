0ad2a077110c20aea52a01466fc09bdfb5bb54d2
Ahmed Hadrich <124152173+ahmedhadrich@users.noreply.github.com>
2025-06-16 08:23:48 +0200

    Add files via upload

---
+++ hospitalListEmbed.js
@@ -0,0 +1,21 @@
+(function () {
+  var containerId = 'hospital-list-container';
+  var src = 'hospital-list.html'; // update path if hosted elsewhere
+
+  // Find or create the container
+  var container = document.getElementById(containerId);
+  if (!container) {
+    container = document.createElement('div');
+    container.id = containerId;
+    document.currentScript.parentNode.insertBefore(container, document.currentScript);
+  }
+
+  // Fetch the external HTML and inject
+  fetch(src)
+    .then(function (response) { return response.text(); })
+    .then(function (html) { container.innerHTML = html; })
+    .catch(function (err) {
+      console.error('Unable to load hospital list:', err);
+      container.innerHTML = '<p>Unable to load hospital list. Please try again later.</p>';
+    });
+})();
